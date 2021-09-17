ca0573105c910dd84d867a3362a6705e680f1461
minch-yoda <minchenko.dev@gmail.com>
2021-09-17 15:47:13 +0300

    fixed install.bat / uninstall.reg to add/remove context menu options to all files and folders

--- install.bat
+++ install.bat
@@ -4,9 +4,9 @@ if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 cd /D %~dp0
 
 :\"%%1\"
-REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDCopyDates" /t REG_SZ /d "Dates COPY" /f
-REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDCopyDates\command" /t REG_SZ /d "%cd%\CopyDates.ps1" /f
+REG ADD "HKEY_CLASSES_ROOT\*\shell\WinCPDCopyDates" /t REG_SZ /d "Dates COPY" /f
+REG ADD "HKEY_CLASSES_ROOT\*\shell\WinCPDCopyDates\command" /t REG_SZ /d "%cd%\CopyDates.ps1" /f
 
-REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDPasteDates" /t REG_SZ /d "Dates PASTE" /f
-REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDPasteDates\command" /t REG_SZ /d "%cd%\PasteDates.ps1" /f
+REG ADD "HKEY_CLASSES_ROOT\*\shell\WinCPDPasteDates" /t REG_SZ /d "Dates PASTE" /f
+REG ADD "HKEY_CLASSES_ROOT\*\shell\WinCPDPasteDates\command" /t REG_SZ /d "%cd%\PasteDates.ps1" /f
 pause
