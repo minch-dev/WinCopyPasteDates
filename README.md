e411c303c0de1558bcc54fef7f3086c8992ed857
minch-yoda <minchenko.dev@gmail.com>
2021-09-17 12:49:51 +0300

    install.bat automatically requests admin privileges, added two commands Dates COPY and Dates PASTE

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # WinCopyPasteDates
 Windows context menu item that allows you to copy any file date to clipboard and then apply it to any other file.
-Written as simple powershell .ps1 script, a .bat installer and a .reg file for uninstallation
+Written as simple powershell .ps1 scripts, a .bat installer and a .reg file for uninstallation
--- install.bat
+++ install.bat
@@ -1,1 +1,10 @@
-REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCopyPasteDates" /v @ /t REG_SZ /d %cd%/WinCopyPasteDates.ps1 /f
+@echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+cd /D %~dp0
+
+REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDCopyDates" /t REG_SZ /d "Dates COPY" /f
+REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDCopyDates\command" /t REG_SZ /d "%cd%\CopyDates.ps1" /f
+
+REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDPasteDates" /t REG_SZ /d "Dates PASTE" /f
+REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDPasteDates\command" /t REG_SZ /d "%cd%\PasteDates.ps1" /f
+pause
