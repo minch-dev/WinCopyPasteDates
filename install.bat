1e92102d0dfec7bf988bd8bb026bd3c48e5aa030
minch-yoda <minchenko.dev@gmail.com>
2021-09-17 14:07:31 +0300

    looks like my code only adds context menu for folders, need a fix

--- install.bat
+++ install.bat
@@ -1,7 +1,9 @@
+:by toster-cx@stackoverflow
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 cd /D %~dp0
 
+:\"%%1\"
 REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDCopyDates" /t REG_SZ /d "Dates COPY" /f
 REG ADD "HKEY_CLASSES_ROOT\Directory\shell\WinCPDCopyDates\command" /t REG_SZ /d "%cd%\CopyDates.ps1" /f
 
