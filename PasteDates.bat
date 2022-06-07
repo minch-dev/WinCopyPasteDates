eb93c61f7109ae2974568acec75d42a0b5dfdb3e
minch-yoda <minchenko.dev@gmail.com>
2022-06-08 02:12:53 +0300

    1.2.3 fixed problem with & (and probably other 'special' symbols) in file and folder names

--- PasteDates.bat
+++ PasteDates.bat
@@ -2,8 +2,8 @@ chcp 65001
 echo off
 SETLOCAL
 set FILENAME="%~nx1"
-set FOLDER=%~dp1
-set FOLDER=%FOLDER:~0,-1%
+set FOLDER="%~dp1
+set FOLDER=%FOLDER:~0,-1%"
 
 for %%a in ("%TMP%\WinCPD\*.DATES") do (
 set DATES=%%~nxa
@@ -12,7 +12,7 @@ set DATES=%%~nxa
 IF defined DATES (
  cd /d %TMP%\WinCPD
  rename "%DATES%" %FILENAME%
- robocopy "." "%FOLDER%" %FILENAME% /DCOPY:X /COPY:T  /R:0
+ robocopy "." %FOLDER% %FILENAME% /DCOPY:X /COPY:T  /R:0
  rename %FILENAME% "%DATES%"
 )
 ENDLOCAL
